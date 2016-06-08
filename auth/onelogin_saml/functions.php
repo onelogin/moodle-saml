25e0dda64276acc6df8a1bfc2dd236566205d7a6
Sixto Martin <pitbulk@gmail.com>
2016-06-08 13:47:43 +0200

    Add generate password method

--- auth/onelogin_saml/functions.php
+++ auth/onelogin_saml/functions.php
@@ -115,7 +115,7 @@
 
 		// ensure that only saml auth module is chosen
 		$authsenabled = get_enabled_auth_plugins();
-		$password = time();
+		$password = randomPassword();
 		$created = false;
 
 		if ($user = get_complete_user_data($saml_account_matcher, $user_saml[$saml_account_matcher])) {
@@ -339,5 +339,16 @@
 		}
 		session_destroy();
 		ob_clean();
+	}
+
+	function randomPassword() {
+		$alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+		$pass = array();
+		$alphaLength = strlen($alphabet) - 1;
+		for ($i = 0; $i < 12; $i++) {
+			$n = rand(0, $alphaLength);
+			$pass[] = $alphabet[$n];
+		}
+		return implode($pass);
 	}
 
