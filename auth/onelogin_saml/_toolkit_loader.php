4158afb2ebe6f521b45e8cc029360953c5e8fb7e
Sixto Martin <pitbulk@gmail.com>
2016-06-27 14:18:13 +0200

    Release version 2.2.0

--- auth/onelogin_saml/_toolkit_loader.php
+++ auth/onelogin_saml/_toolkit_loader.php
@@ -1,6 +1,6 @@
 <?php  
 /*********************************************************************************
-Copyright (c) 2010-2014, OneLogin, Inc.
+Copyright (c) 2010-2016, OneLogin, Inc.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
